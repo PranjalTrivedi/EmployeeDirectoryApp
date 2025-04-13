1b74c1bef5ad2a376b3023ea2ebec259ab390ffa
PranjalTrivedi <pranjaltrivedi75@gmail.com>
2025-04-13 13:31:36 -0400

    Initial Commit

---
+++ EmployeeDirectoryApp/EmployeeDirectoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirectoryAppApp.swift
+//  EmployeeDirectoryApp
+//
+//  Created by Pranjal Trivedi on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDirectoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmployeeDirectoryAppTests/EmployeeDirectoryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirectoryAppTests.swift
+//  EmployeeDirectoryAppTests
+//
+//  Created by Pranjal Trivedi on 2025-04-13.
+//
+
+import Testing
+@testable import EmployeeDirectoryApp
+
+struct EmployeeDirectoryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
